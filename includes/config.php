3d6fa7081dd575cc1adf3a291c4e74de840971c2
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-05-22 14:17:48 -0400

    credenciales base de datos

--- includes/config.php
+++ includes/config.php
@@ -10,7 +10,7 @@
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
   define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_PASS', '1010Will*_' );             // Set database password
   define( 'DB_NAME', 'prueba' );        // Set database name
 
 ?>
